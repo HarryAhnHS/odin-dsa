069eed123b2d76bfa432a04e2688f21fd1897e16
HarryAhnHS <hongsuhahn@gmail.com>
2024-06-14 14:40:28 +0800

    basic one dimensional hashmap - replace key,value if collision

--- hashmap/index.js
+++ hashmap/index.js
@@ -10,16 +10,19 @@ class HashMap {
         this.table = new Array(16).fill(null);
         this.load_factor = 0.75;
         this.capacity = this.table.length;
-        this.loaded = 0;
+    }
+
+    getTable() {
+        return this.table;
     }
 
     hash(key) {
         let hashCode = 0;
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
-        return hashCode % this.capacity;
+        return hashCode;
     }
 
     resize() {
@@ -28,27 +31,33 @@ class HashMap {
         this.table = new Array(this.capacity).fill(null);
         old.forEach((bucket) => {
             if (bucket != null) {
-                set(bucket.key,bucket.val);
+                this.set(bucket.key, bucket.val);
             }
         })
+
+        console.log('resizing to :' + this.capacity);
 
     }
 
     set(key, value) {
+        this.table[this.hash(key)] = new Node(key,value);
+        console.log(key + ' hashed to ' + this.hash(key));
+
         // Grow if capacity is loaded
-        if (this.loaded > this.load_factor * this.capacity) {
+        if (this.length() > this.load_factor * this.capacity) {
             this.resize();
         }
-        this.table[this.hash(key)] = new Node(key,value);
-        this.loaded++;
     }
 
     get(key) {
-        return this.table[this.hash(key)];
+        if (this.has(key)) {
+            return this.table[this.hash(key)].val;
+        }
+        else return null;
     }
 
     has(key) {
-        return (this.table[this.hash(key)] != null);
+        return (this.table[this.hash(key)] != null && this.table[this.hash(key)].key == key);
     }
 
     remove(key) {
@@ -60,7 +69,13 @@ class HashMap {
     }
 
     length() {
-        return this.loaded;
+        let cnt = 0;
+        this.table.forEach((bucket) => {
+            if (bucket != null) {
+                cnt++;
+            }
+        })
+        return cnt;
     }
 
     clear() {
@@ -83,26 +98,22 @@ class HashMap {
         let valuesArray = [];
         this.table.forEach((bucket) => {
             if (bucket != null) {
-                keysArray.push(bucket.val);
+                valuesArray.push(bucket.val);
             }
         })
+        return valuesArray;
     }
 
     entries() {
         let entriesArray = [];
         this.table.forEach((bucket) => {
             let entry = [];
-            if (bucket != null) {
+            if (bucket !== null) {
                 entry.push(bucket.key);
                 entry.push(bucket.val);
+                entriesArray.push(entry);
             }
-            entriesArray.push(entry);
         })
         return entriesArray;
     }
 }
-
-let hashmap = new HashMap();
-console.log(hashmap.keys());
-console.log(hashmap.entries());
-console.log(hashmap.entries());
